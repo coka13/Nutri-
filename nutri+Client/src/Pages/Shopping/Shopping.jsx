a623f451ca43eccac6bcb063ed1bba402f1c8784
Bilal Nasir <bilalogist@gmail.com>
2024-07-16 20:50:26 +0500

    shopping

--- nutri+Client/src/Pages/Shopping/Shopping.jsx
+++ nutri+Client/src/Pages/Shopping/Shopping.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Button,
   Modal,
@@ -11,23 +11,27 @@ import {
   Select,
   FormControl,
   InputLabel,
-} from '@mui/material';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
-import './Shopping.css';
+} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import "./Shopping.css";
+import { fetchAllRecipes } from "../store/slices/recipesSlice";
 
 const Shopping = () => {
-  const [foodName, setFoodName] = useState('');
-  const [quantity, setQuantity] = useState('');
-  const [unit, setUnit] = useState('');
+  const [foodName, setFoodName] = useState("");
+  const [shoppingListName, setShoppingListName] = useState("");
+  const [selectedFood, setSelectedFood] = useState();
+  const [quantity, setQuantity] = useState("");
+  const [unit, setUnit] = useState("");
   const [openModal, setOpenModal] = useState(false);
   const [foodList, setFoodList] = useState([]);
   const [totalQuantity, setTotalQuantity] = useState(0);
   const [shoppingLists, setShoppingLists] = useState([]);
-  const [selectedShoppingList, setSelectedShoppingList] = useState('');
+  const [selectedShoppingList, setSelectedShoppingList] = useState([]);
   const darkMode = useSelector((state) => state.darkMode.darkMode);
   const userID = useSelector((state) => state.auth.user._id);
-
+  const recipes = useSelector((state) => state.recipes.recipes);
+  const dispatch = useDispatch();
   // Modal handlers
   const handleModalOpen = () => {
     setOpenModal(true);
@@ -39,40 +43,38 @@ const Shopping = () => {
   };
 
   const resetForm = () => {
-    setFoodName('');
-    setQuantity('');
-    setUnit('');
+    setFoodName("");
+    setQuantity("");
+    setUnit("");
   };
 
   // Handle form submission
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Parse quantity as integer
-    const parsedQuantity = parseInt(quantity, 10);
+    const items=[];
+    selectedShoppingList.forEach((rec)=>{
+      items.push(rec.ingredients)
+    })
 
-    const newFoodItem = {
-      foodName: foodName,
-      quantity: parsedQuantity,
-      unit: unit,
-    };
-
-    setFoodList([...foodList, newFoodItem]);
-    setTotalQuantity(totalQuantity + parsedQuantity);
-
-    resetForm();
-    setOpenModal(false);
+    const res=await axios.post(`http://localhost:3000/api/shopping/${userID}/shoppingLists`,{
+      name:shoppingListName,
+      items:items
+    })
   };
 
   // Fetch all shopping lists for the user
   useEffect(() => {
+    dispatch(fetchAllRecipes());
     const fetchShoppingLists = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/api/shopping/${userID}/shoppingLists`);
+        const response = await axios.get(
+          `http://localhost:3000/api/shopping/${userID}/shoppingLists`
+        );
         setShoppingLists(response.data);
-        console.log('Shopping lists fetched:', response.data);
+        console.log("Shopping lists fetched:", response.data);
       } catch (error) {
-        console.error('Failed to fetch shopping lists:', error);
+        console.error("Failed to fetch shopping lists:", error);
       }
     };
     fetchShoppingLists();
@@ -80,33 +82,39 @@ const Shopping = () => {
 
   // Save shopping list
   const saveShoppingList = async () => {
-    
     try {
-      const response = await axios.post(`http://localhost:3000/api/shopping/${userID}/shoppingLists`, {
-        foodList: foodList,
-        name: 'Shopping List', // Set a default name for the shopping list
-      });
-      console.log('Shopping list saved:', response.data);
+      const response = await axios.post(
+        `http://localhost:3000/api/shopping/${userID}/shoppingLists`,
+        {
+          foodList: foodList,
+          name: "Shopping List", // Set a default name for the shopping list
+        }
+      );
+      console.log("Shopping list saved:", response.data);
       // Optionally update local state or reset form after saving
       setFoodList([]);
       setTotalQuantity(0);
     } catch (error) {
-      console.error('Failed to save shopping list:', error);
+      console.error("Failed to save shopping list:", error);
     }
   };
 
   // Delete shopping list
   const deleteShoppingList = async (shoppingListId) => {
     try {
-      await axios.delete(`http://localhost:3000/api/shopping/${userID}/shoppingLists/${shoppingListId}`);
-      console.log('Shopping list deleted successfully.');
+      await axios.delete(
+        `http://localhost:3000/api/shopping/${userID}/shoppingLists/${shoppingListId}`
+      );
+      console.log("Shopping list deleted successfully.");
       // Optionally update local state or fetch updated list after deletion
-      const updatedLists = shoppingLists.filter((list) => list._id !== shoppingListId);
+      const updatedLists = shoppingLists.filter(
+        (list) => list._id !== shoppingListId
+      );
       setShoppingLists(updatedLists);
-      setSelectedShoppingList(''); // Clear selected shopping list after deletion
+      setSelectedShoppingList(""); // Clear selected shopping list after deletion
       setFoodList([]); // Clear food list when deleting a list
     } catch (error) {
-      console.error('Failed to delete shopping list:', error);
+      console.error("Failed to delete shopping list:", error);
     }
   };
 
@@ -116,7 +124,9 @@ const Shopping = () => {
     setSelectedShoppingList(selectedListId);
 
     // Fetch or update foodList based on selected shopping list
-    const selectedList = shoppingLists.find((list) => list._id === selectedListId);
+    const selectedList = shoppingLists.find(
+      (list) => list._id === selectedListId
+    );
 
     if (selectedList) {
       setFoodList(selectedList.foodItems); // Assuming selectedList.foodList contains items
@@ -125,6 +135,12 @@ const Shopping = () => {
     }
   };
 
+  const handleFoodSelection=(id)=>{
+    setSelectedFood(id)
+    const tempList=[...selectedShoppingList];
+    tempList.push(recipes.find(recipe=>recipe._id===id))
+    setSelectedShoppingList(tempList)
+  }
   return (
     <div className="shopping-container">
       <Button
@@ -132,11 +148,11 @@ const Shopping = () => {
         variant="contained"
         color="primary"
         sx={{
-          marginTop: '10px',
-          marginBottom: '10px',
-          backgroundColor: '#B81D33',
-          '&:hover': {
-            backgroundColor: '#B81D33',
+          marginTop: "10px",
+          marginBottom: "10px",
+          backgroundColor: "#B81D33",
+          "&:hover": {
+            backgroundColor: "#B81D33",
           },
         }}
         onClick={handleModalOpen}
@@ -154,61 +170,39 @@ const Shopping = () => {
         }}
       >
         <div className="modal-content">
-          <Typography variant="h6" component="h2" gutterBottom className="modal-title">
+          <Typography
+            variant="h6"
+            component="h2"
+            gutterBottom
+            className="modal-title"
+          >
             Enter Food Details
           </Typography>
           <form onSubmit={handleSubmit}>
-            <TextField
+          <TextField
               type="text"
-              value={foodName}
-              onChange={(e) => setFoodName(e.target.value)}
-              label="Food Name"
+              value={shoppingListName}
+              onChange={(e) => setShoppingListName(e.target.value)}
+              label="Shooping List Name"
               fullWidth
               variant="outlined"
               margin="normal"
-              required
+              className="input-field"
               sx={{
-                marginBottom: '10px',
-                '& .MuiInputLabel-root.Mui-focused': {
-                  color: 'black',
-                  fontWeight: 'bold',
+                marginBottom: "10px",
+                "& .MuiInputLabel-root.Mui-focused": {
+                  color: "black",
+                  fontWeight: "bold",
                 },
-                '& .MuiOutlinedInput-root': {
-                  '& fieldset': {
-                    borderColor: '#B81D33',
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": {
+                    borderColor: "#B81D33",
                   },
-                  '&:hover fieldset': {
-                    borderColor: '#B81D33',
+                  "&:hover fieldset": {
+                    borderColor: "#B81D33",
                   },
-                  '&.Mui-focused fieldset': {
-                    borderColor: '#B81D33',
-                  },
-                },
-              }}
-            />
-            <TextField
-              type="number"
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-              label="Quantity"
-              fullWidth
-              variant="outlined"
-              margin="normal"
-              required
-              sx={{
-                '& .MuiInputLabel-root.Mui-focused': {
-                  color: 'black',
-                  fontWeight: 'bold',
-                },
-                '& .MuiOutlinedInput-root': {
-                  '& fieldset': {
-                    borderColor: '#B81D33',
-                  },
-                  '&:hover fieldset': {
-                    borderColor: '#B81D33',
-                  },
-                  '&.Mui-focused fieldset': {
-                    borderColor: '#B81D33',
+                  "&.Mui-focused fieldset": {
+                    borderColor: "#B81D33",
                   },
                 },
               }}
@@ -219,83 +213,118 @@ const Shopping = () => {
               margin="normal"
               required
               sx={{
-                '& .MuiInputLabel-root.Mui-focused': {
-                  color: 'black',
-                  fontWeight: 'bold',
+                "& .MuiInputLabel-root.Mui-focused": {
+                  color: "black",
+                  fontWeight: "bold",
                 },
-                '& .MuiOutlinedInput-root': {
-                  '& fieldset': {
-                    borderColor: '#B81D33',
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": {
+                    borderColor: "#B81D33",
                   },
-                  '&:hover fieldset': {
-                    borderColor: '#B81D33',
+                  "&:hover fieldset": {
+                    borderColor: "#B81D33",
                   },
-                  '&.Mui-focused fieldset': {
-                    borderColor: '#B81D33',
+                  "&.Mui-focused fieldset": {
+                    borderColor: "#B81D33",
                   },
                 },
               }}
             >
-              <InputLabel id="unit-label">Unit</InputLabel>
+              <InputLabel id="unit-label">Select Food</InputLabel>
               <Select
                 labelId="unit-label"
                 id="unit"
-                label="Unit"
-                value={unit}
-                onChange={(e) => setUnit(e.target.value)}
+                label="Select Food"
+                value={selectedFood}
+                onChange={(e) => handleFoodSelection(e.target.value)}
                 sx={{
-                  '& .MuiInputLabel-root.Mui-focused': {
-                    color: 'black',
-                    fontWeight: 'bold',
+                  "& .MuiInputLabel-root.Mui-focused": {
+                    color: "black",
+                    fontWeight: "bold",
                   },
-                  '& .MuiOutlinedInput-root': {
-                    '& fieldset': {
-                      borderColor: '#B81D33',
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderColor: "#B81D33",
                     },
-                    '&:hover fieldset': {
-                      borderColor: '#B81D33',
+                    "&:hover fieldset": {
+                      borderColor: "#B81D33",
                     },
-                    '&.Mui-focused fieldset': {
-                      borderColor: '#B81D33',
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#B81D33",
                     },
                   },
                 }}
               >
-                <MenuItem value="">Select Unit</MenuItem>
-                <MenuItem value="piece">piece</MenuItem>
+                {recipes?.length > 0 &&
+                  recipes.map((r) => {
+                    return <MenuItem value={r._id}>{r.recipeName}</MenuItem>;
+                  })}
+                {/* <MenuItem value="piece">piece</MenuItem>
                 <MenuItem value="kg">kg</MenuItem>
                 <MenuItem value="liter">liter</MenuItem>
                 <MenuItem value="gram">gram</MenuItem>
-                <MenuItem value="milliliter">milliliter</MenuItem>
+                <MenuItem value="milliliter">milliliter</MenuItem> */}
               </Select>
             </FormControl>
+            {selectedShoppingList?.length>0 && (
+          <List>
+            {selectedShoppingList.map((food, index) => (
+              <ListItem key={index}>
+                <ListItemText
+                  primary={
+                    <span style={{ color: darkMode ? "#fff" : "black" }}>
+                      {food.recipeName}
+                    </span>
+                  }
+                  secondary={
+                    <span style={{ color: darkMode ? "white" : "grey" }}>
+                      {food.ingredients.map((ing)=>{
+                        return `${ing.ingredient} ${ing.quantity} ${ing.unit}`
+                      })}
+                      
+                    </span>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        )}
+
             <Button
               type="submit"
               variant="contained"
               color="primary"
               sx={{
-                marginTop: '10px',
-                marginBottom: '10px',
-                backgroundColor: '#B81D33',
-                '&:hover': {
-                  backgroundColor: '#B81D33',
+                marginTop: "10px",
+                marginBottom: "10px",
+                backgroundColor: "#B81D33",
+                "&:hover": {
+                  backgroundColor: "#B81D33",
                 },
               }}
             >
-              Add Food
+              Add to Shopping List
             </Button>
           </form>
         </div>
       </Modal>
 
       <div className="shopping-list">
-        {selectedShoppingList && (
+        {selectedShoppingList?.length>0 && (
           <List>
             {foodList.map((food, index) => (
               <ListItem key={index}>
                 <ListItemText
-                  primary={<span style={{ color: darkMode ? '#fff' : 'black' }}>{food.foodName}</span>}
-                  secondary={<span style={{ color: darkMode ? 'white' : 'grey' }}>{food.quantity} {food.unit}</span>}
+                  primary={
+                    <span style={{ color: darkMode ? "#fff" : "black" }}>
+                      {food.foodName}
+                    </span>
+                  }
+                  secondary={
+                    <span style={{ color: darkMode ? "white" : "grey" }}>
+                      {food.quantity} {food.unit}
+                    </span>
+                  }
                 />
               </ListItem>
             ))}
@@ -303,7 +332,9 @@ const Shopping = () => {
         )}
         {shoppingLists.length > 0 && (
           <FormControl fullWidth variant="outlined" margin="normal" required>
-            <InputLabel id="shopping-list-label">Select Shopping List</InputLabel>
+            <InputLabel id="shopping-list-label">
+              Select Shopping List
+            </InputLabel>
             <Select
               labelId="shopping-list-label"
               id="shoppingList"
@@ -311,19 +342,19 @@ const Shopping = () => {
               onChange={handleShoppingListChange}
               label="Shopping List"
               sx={{
-                '& .MuiInputLabel-root.Mui-focused': {
-                  color: 'black',
-                  fontWeight: 'bold',
+                "& .MuiInputLabel-root.Mui-focused": {
+                  color: "black",
+                  fontWeight: "bold",
                 },
-                '& .MuiOutlinedInput-root': {
-                  '& fieldset': {
-                    borderColor: '#B81D33',
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": {
+                    borderColor: "#B81D33",
                   },
-                  '&:hover fieldset': {
-                    borderColor: '#B81D33',
+                  "&:hover fieldset": {
+                    borderColor: "#B81D33",
                   },
-                  '&.Mui-focused fieldset': {
-                    borderColor: '#B81D33',
+                  "&.Mui-focused fieldset": {
+                    borderColor: "#B81D33",
                   },
                 },
               }}
@@ -343,11 +374,11 @@ const Shopping = () => {
         variant="contained"
         color="primary"
         sx={{
-          marginTop: '10px',
-          marginBottom: '10px',
-          backgroundColor: '#B81D33',
-          '&:hover': {
-            backgroundColor: '#B81D33',
+          marginTop: "10px",
+          marginBottom: "10px",
+          backgroundColor: "#B81D33",
+          "&:hover": {
+            backgroundColor: "#B81D33",
           },
         }}
         onClick={saveShoppingList}
@@ -360,11 +391,11 @@ const Shopping = () => {
         variant="contained"
         color="secondary"
         sx={{
-          marginTop: '10px',
-          marginBottom: '10px',
-          backgroundColor: '#B81D33',
-          '&:hover': {
-            backgroundColor: '#B81D33',
+          marginTop: "10px",
+          marginBottom: "10px",
+          backgroundColor: "#B81D33",
+          "&:hover": {
+            backgroundColor: "#B81D33",
           },
         }}
         onClick={() => deleteShoppingList(selectedShoppingList)}
