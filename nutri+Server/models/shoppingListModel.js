6ff02f424a4515c4066e0fe5c32c24c24ba7b2b0
Bilal Nasir <bilalogist@gmail.com>
2024-07-16 20:50:04 +0500

    shopping list

--- nutri+Server/models/shoppingListModel.js
+++ nutri+Server/models/shoppingListModel.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-
+const ShoppingItemSchema = new mongoose.Schema({
+  ingredient: String,
+  quantity: Number, // e.g., 2
+  unit: String // e.g., 'cups', 'tablespoons', etc.
+});
 const shoppingListSchema = new mongoose.Schema({
-  foodItems: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "FoodItem",
-  }],
+  items: [ShoppingItemSchema],
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
